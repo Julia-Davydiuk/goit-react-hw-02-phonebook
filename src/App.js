b0c4d3e6b3ef5cf08f59be7980f1688d785f6e75
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-05-20 11:00:42 +0200

    add alert

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
  
 class App extends Component {
     state = {
-     contacts: [],
+    contacts: [
+     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        filter: '',
     };
 
-    addContact = name => {
+    addContact = ({name, number}) => {
         const contact = {
             id: uuidv4(),
             name,
-        };
-        this.setState(prevState => ({
-            contacts: [contact, ...prevState.contacts],
+            number,
+      };
+      
+      this.state.contacts.find(e => e.name === contact.name) ?
+        alert (`${name} is already in contacts`):
+        this.setState(({ contacts }) => ({
+            contacts: [contact, ...contacts],
         }));
     };
+
+    changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  filterContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
 
     render() {
         return (
@@ -24,6 +55,10 @@ class App extends Component {
                 <h1>Phonebook</h1>
                 <ContactForm onSubmit={this.addContact} />
                 <h2>Contacts</h2>
+                <Filter value={this.state.filter} onChange={this.changeFilter} />
+                <ContactList contactList={this.filterContacts()}
+                  onClick={this.deleteContact}
+                />
             </div>
         )
   }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import style from './ContactForm.module.css';
+import PropTypes from 'prop-types';
 
 class ContactForm extends Component {
     state = {
@@ -14,8 +15,8 @@ class ContactForm extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        this.props.onSubmit(this.state.name);
-        this.setState({ name: '', number: '' });
+        this.props.onSubmit(this.state);
+        this.setState({ name: '', number: ''})
     };
 
     render() {
@@ -24,27 +25,27 @@ class ContactForm extends Component {
                 onSubmit={this.handleSubmit}>
                 <label className={style.label}>
                     Name <input
-                        className={style.input}
+                      className={style.input}
                       type="text"
-                        name="name"
-                        value={this.state.name}
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
                       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                       title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                       required
-                      onChange={this.handleChange}
                     />
                 </label>
 
                 <label className={style.label}>
                     Number <input
-                        className={style.input}
+                      className={style.input}
                       type="tel"
                       name="number"
                       value={this.state.number}
+                      onChange={this.handleChange}
                       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                       title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                       required
-                      onChange={this.handleChange}
                       />
                 </label>
 
@@ -53,5 +54,9 @@ class ContactForm extends Component {
         )
     }
 }
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
